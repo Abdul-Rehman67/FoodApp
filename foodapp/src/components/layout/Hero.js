f740a3a8b649ad0b6c664272666ca3d712a9c631
Abdul Rehman Shaikh <arehmanshaikh@folio3.com>
2024-08-20 02:19:21 +0500

    initial hero and header component

---
+++ foodapp/src/components/layout/Hero.js
@@ -0,0 +1,27 @@
+import RightArrow from "@/app/Icons/RightArrow";
+import Image from "next/image";
+export default function Hero() {
+  return (
+    <secion className="grid grid-cols-2">
+      <div className="py-12">
+        <h1 className="text-4xl font-semibold"> Everything is better with a <span className="text-primary font-semibold">Pizza</span></h1>
+        <p className="my-4 text-gray-500">
+          Pizza is the missing piece that makes every day complete, a simple yet
+          delicious joy in life
+        </p>
+        <div className="flex gap-4 items-center">
+
+        <button className="bg-primary text-white py-2 px-4 rounded-full flex gap-2 items-center">Order Now
+            <RightArrow/>
+        </button>
+        <button className="flex gap-2 text-gray-600 font-semibold">Learn More
+            <RightArrow/>
+        </button>
+        </div>
+      </div>
+      <div className="relative">
+        <Image src={"/pizza.png"} layout="fill" objectFit="contain" />
+      </div>
+    </secion>
+  );
+}
